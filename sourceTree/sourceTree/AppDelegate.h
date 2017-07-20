2e19a616194a0dbd8fa9e86ab79416bc4cefc3c4
brian0516 <brian_0419@hotmail.com>
2017-07-20 15:37:25 +0800

    初始版本

---
+++ sourceTree/sourceTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sourceTree
+//
+//  Created by shuanglong on 17/7/20.
+//  Copyright © 2017年 shuanglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
